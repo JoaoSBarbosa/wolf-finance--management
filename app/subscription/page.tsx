71a3627533f7b582031508150778ff5a9261d185
joao-wsl <contato.jsbarbosa@gmail.com>
2024-11-05 12:59:48 -0300

    :tada: chore: add prisma and database setup

---
+++ app/subscription/page.tsx
@@ -0,0 +1,7 @@
+const Subscription = () => {
+  return (  
+    <h1>Página Inscrever-se</h1>
+  );
+}
+ 
+export default Subscription;
---
+++ app/transactions/page.tsx
@@ -0,0 +1,7 @@
+const Transactions = () => {
+  return ( 
+    <h1>Página Transação</h1>
+   );
+}
+ 
+export default Transactions;
